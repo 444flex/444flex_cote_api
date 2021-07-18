61a125544d664809d69371ba2087d7e87409bbe4
jb <jb@192.168.0.36>
2021-07-18 15:02:34 +0900

    [JB] 에러코드 변경

--- src/main/java/com/flex/api/exception/EntityNotModifyException.java
+++ src/main/java/com/flex/api/exception/EntityNotModifyException.java
@@ -8,7 +8,7 @@ public class EntityNotModifyException extends ServiceException {
 	static final HttpStatus RESPONSE_CODE = HttpStatus.UNPROCESSABLE_ENTITY;
 	static final ErrorType ERROR_TYPE = ErrorType.ClientSystem;
 	static final ErrorReason ERROR_REASON = ErrorReason.EntityNotModify;
-	static final int ERROR_CODE = 4004;
+	static final int ERROR_CODE = 4003;
 	
 	public EntityNotModifyException(String errorTarget, String errorMessage, Throwable cause) {
 		super(RESPONSE_CODE, ERROR_TYPE, ERROR_REASON, ERROR_CODE, errorTarget, errorMessage, cause);
